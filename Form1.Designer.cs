7bde9c093e52ade151decba3c201d775914975d0
Ossyence <ossyence@gmail.com>
2022-10-28 21:40:09 +0200

    Source code

---
+++ Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿
+namespace Roblox_Catalog_Items_Grabber
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.title = new System.Windows.Forms.Label();
+            this.button_run = new System.Windows.Forms.Button();
+            this.button_copy = new System.Windows.Forms.Button();
+            this.progress_running = new System.Windows.Forms.Label();
+            this.progress_done = new System.Windows.Forms.Label();
+            this.debugMode_box = new System.Windows.Forms.CheckBox();
+            this.topIcon = new System.Windows.Forms.PictureBox();
+            this.result_text = new System.Windows.Forms.Label();
+            this.button_cancel = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.topIcon)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // title
+            // 
+            this.title.BackColor = System.Drawing.Color.White;
+            this.title.Font = new System.Drawing.Font("Microsoft YaHei UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.title.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.title.Location = new System.Drawing.Point(69, 3);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(321, 28);
+            this.title.TabIndex = 0;
+            this.title.Text = "Roblox Catalog Item Grabber";
+            // 
+            // button_run
+            // 
+            this.button_run.BackColor = System.Drawing.Color.White;
+            this.button_run.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.button_run.Location = new System.Drawing.Point(277, 411);
+            this.button_run.Name = "button_run";
+            this.button_run.Size = new System.Drawing.Size(400, 34);
+            this.button_run.TabIndex = 10;
+            this.button_run.Text = "Run (may take a while)";
+            this.button_run.UseVisualStyleBackColor = false;
+            this.button_run.Click += new System.EventHandler(this.button_run_Click);
+            // 
+            // button_copy
+            // 
+            this.button_copy.BackColor = System.Drawing.Color.White;
+            this.button_copy.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.button_copy.Location = new System.Drawing.Point(683, 411);
+            this.button_copy.Name = "button_copy";
+            this.button_copy.Size = new System.Drawing.Size(112, 34);
+            this.button_copy.TabIndex = 11;
+            this.button_copy.Text = "Copy Result";
+            this.button_copy.UseVisualStyleBackColor = false;
+            this.button_copy.Click += new System.EventHandler(this.button_copy_Click);
+            // 
+            // progress_running
+            // 
+            this.progress_running.BackColor = System.Drawing.Color.White;
+            this.progress_running.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.progress_running.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.progress_running.Location = new System.Drawing.Point(419, 31);
+            this.progress_running.Name = "progress_running";
+            this.progress_running.Size = new System.Drawing.Size(376, 19);
+            this.progress_running.TabIndex = 13;
+            this.progress_running.Text = "Running: null/null, null";
+            this.progress_running.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // progress_done
+            // 
+            this.progress_done.BackColor = System.Drawing.Color.White;
+            this.progress_done.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.progress_done.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.progress_done.Location = new System.Drawing.Point(423, 12);
+            this.progress_done.Name = "progress_done";
+            this.progress_done.Size = new System.Drawing.Size(372, 19);
+            this.progress_done.TabIndex = 14;
+            this.progress_done.Text = "Done: null/null";
+            this.progress_done.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // debugMode_box
+            // 
+            this.debugMode_box.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.debugMode_box.Location = new System.Drawing.Point(74, 31);
+            this.debugMode_box.Name = "debugMode_box";
+            this.debugMode_box.Size = new System.Drawing.Size(173, 19);
+            this.debugMode_box.TabIndex = 15;
+            this.debugMode_box.Text = "Debug Mode";
+            this.debugMode_box.UseVisualStyleBackColor = true;
+            this.debugMode_box.CheckedChanged += new System.EventHandler(this.debugMode_box_CheckedChanged);
+            // 
+            // topIcon
+            // 
+            this.topIcon.BackColor = System.Drawing.Color.Transparent;
+            this.topIcon.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.topIcon.ErrorImage = null;
+            this.topIcon.Image = ((System.Drawing.Image)(resources.GetObject("topIcon.Image")));
+            this.topIcon.InitialImage = null;
+            this.topIcon.Location = new System.Drawing.Point(3, 3);
+            this.topIcon.Name = "topIcon";
+            this.topIcon.Size = new System.Drawing.Size(60, 59);
+            this.topIcon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.topIcon.TabIndex = 16;
+            this.topIcon.TabStop = false;
+            // 
+            // result_text
+            // 
+            this.result_text.BackColor = System.Drawing.Color.White;
+            this.result_text.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.result_text.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.result_text.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.result_text.Location = new System.Drawing.Point(3, 65);
+            this.result_text.Name = "result_text";
+            this.result_text.Size = new System.Drawing.Size(792, 343);
+            this.result_text.TabIndex = 0;
+            this.result_text.Text = "Press run to start!";
+            // 
+            // button_cancel
+            // 
+            this.button_cancel.BackColor = System.Drawing.Color.White;
+            this.button_cancel.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.button_cancel.Location = new System.Drawing.Point(3, 411);
+            this.button_cancel.Name = "button_cancel";
+            this.button_cancel.Size = new System.Drawing.Size(268, 34);
+            this.button_cancel.TabIndex = 17;
+            this.button_cancel.Text = "Cancel";
+            this.button_cancel.UseVisualStyleBackColor = false;
+            this.button_cancel.Click += new System.EventHandler(this.button_cancel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.GhostWhite;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button_cancel);
+            this.Controls.Add(this.result_text);
+            this.Controls.Add(this.topIcon);
+            this.Controls.Add(this.debugMode_box);
+            this.Controls.Add(this.progress_done);
+            this.Controls.Add(this.progress_running);
+            this.Controls.Add(this.button_copy);
+            this.Controls.Add(this.button_run);
+            this.Controls.Add(this.title);
+            this.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.ImeMode = System.Windows.Forms.ImeMode.On;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Text = "Roblox Catalog Grabber";
+            ((System.ComponentModel.ISupportInitialize)(this.topIcon)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label title;
+        private System.Windows.Forms.Button button_run;
+        private System.Windows.Forms.Button button_copy;
+        public System.Windows.Forms.Label progress_running;
+        public System.Windows.Forms.Label progress_done;
+        private System.Windows.Forms.CheckBox debugMode_box;
+        private System.Windows.Forms.PictureBox topIcon;
+        private System.Windows.Forms.Label result_text;
+        private System.Windows.Forms.Button button_cancel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,418 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//using System.Text.Json;
+using Newtonsoft.Json;
+using System.Threading;
+using System.Net.Http;
+using System.Net;
+
+namespace Roblox_Catalog_Items_Grabber
+{
+    public partial class Form1 : Form
+    {
+        bool debugMode = false;
+        bool running = false;
+        bool cancelled = false;
+
+        String dataFinished = "nil";
+        
+        String[] accessories = new String[] { //Accessories
+            "", //Head
+            "", //Face
+            "", //Neck
+            "", //Shoulder
+            "", //Front
+            "", //Back
+            "", //Waist
+        };
+        String[] layeredClothing = new String[] { //Layered Clothing
+            "", //Sweaters
+            "", //Jackets
+            "", //Pants
+            "", //Shorts
+            "", //Dresses & Skirts
+            "", //Shoes
+            "", //Shirt
+            "", //T-Shirt
+        };
+        String[] body = new String[] { //Body
+            "", //Animation Packs
+            "", //Bundles
+            "", //Hair
+            "", //Faces
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string filterString(string str)
+        {
+            char[] allowed = new char[]
+            {
+                '.',',','/','!','@','#','$','%','^','&','*','(',')',
+                '+','-','{','}','[',']',':',';','_','=',' ',
+            };
+
+            StringBuilder filtered = new StringBuilder();
+
+            foreach (char c in str)
+            {
+                bool isValid = false;
+
+                foreach (char c_ in allowed)
+                {
+                    if (c_ == c)
+                    {
+                        isValid = true;
+                    }
+                }
+
+                if ((c >= '0' && c <= '9') || (c >= 'A' && c <= 'z') || isValid)
+                {
+                    filtered.Append(c);
+                }
+            }
+
+            return filtered.ToString();
+        }
+
+        void run()
+        {
+            running = true;
+
+            int currentNumber = 1;
+            int currentCategory = 0;
+            int currentSubCategory = 0;
+            int currentPage = 0;
+
+            int max = accessories.Length + layeredClothing.Length + body.Length;
+            bool stalling = false;
+            bool finished = false;
+            int stallCounter = 0;
+
+            void getAndCompileDataInCategory(int category, int subCategory, int mainList, int slotInList, int designatedNumber)
+            {
+                while (currentNumber != designatedNumber) { Thread.Sleep(100); }
+                
+                WebClient client = new WebClient();
+
+                String[] compiled = new String[] { };
+                String nextPageCursor = "";
+
+                client.InitializeLifetimeService();
+
+                int runTime = 0;
+
+                void loop() {
+                    while (true)
+                    {
+                        if (cancelled)
+                        {
+                            break;
+                        }
+
+                        try
+                        {
+                            var json = "";
+
+                            if (nextPageCursor == "")
+                            {
+                                if (subCategory != 0) {
+                                    json = client.DownloadString("https://catalog.roblox.com/v1/search/items/details?Category=" + category as string + "&Subcategory=" + subCategory as string + "&Limit=30");
+                                }
+                                else
+                                {
+                                    json = client.DownloadString("https://catalog.roblox.com/v1/search/items/details?Category=" + category as string + "&Limit=30");
+                                }
+                            }
+                            else
+                            {
+                                if (subCategory != 0)
+                                {
+                                    json = client.DownloadString("https://catalog.roblox.com/v1/search/items/details?Category=" + category as string + "&Subcategory=" + subCategory as string + "&Limit=30&cursor=" + nextPageCursor);
+                                }
+                                else
+                                {
+                                    json = client.DownloadString("https://catalog.roblox.com/v1/search/items/details?Category=" + category as string + "&Limit=30&cursor=" + nextPageCursor);
+                                }
+                            }
+
+                            currentSubCategory = subCategory;
+                            currentPage = runTime;
+                            currentCategory = category;
+
+                            dynamic jsonObj = JsonConvert.DeserializeObject(json);
+
+                            if (nextPageCursor != null && nextPageCursor != "")
+                            {
+                                Console.WriteLine("Reading through page: " + runTime as string + ", Category: " + category as string + ", Sub-Category: " + subCategory as string);
+                            }
+
+                            nextPageCursor = jsonObj["nextPageCursor"];
+
+                            String concat = "";
+
+                            for (int i = 0; i < jsonObj["data"].Count; i++)
+                            {
+                                if (jsonObj["data"][i] != null)
+                                {
+                                    string name = filterString((String)jsonObj["data"][i]["name"]);
+                                    String price = "0";
+
+                                    if ((String)jsonObj["data"][i]["price"] != null)
+                                    {
+                                        price = (String)jsonObj["data"][i]["price"];
+                                    }
+                                    else
+                                    {
+                                        price = "0";
+                                    }
+
+                                    concat += "{Name = \"" + name + "\", Id = " + (String)jsonObj["data"][i]["id"] + ", Price = " + price + "}, \n";
+                                }
+                            }
+
+                            if (mainList == 0)
+                            {
+                                accessories[slotInList] += concat;
+                            }
+                            else if (mainList == 1)
+                            {
+                                layeredClothing[slotInList] += concat;
+                            }
+                            else if (mainList == 2)
+                            {
+                                body[slotInList] += concat;
+                            }
+
+                            runTime += 1;
+                            stalling = false;
+
+                            if (debugMode)
+                            {
+                                break;
+                            }
+
+                            if (nextPageCursor == null)
+                            {
+                                break;
+                            }
+                        }
+                        catch (WebException e) { stalling = true; }
+
+                        client.Dispose();
+                        client.Proxy = null;
+
+                        Thread.Sleep(500);
+                    }
+
+                    currentNumber += 1;
+
+                    if (currentNumber > max)
+                    {
+                        finished = true;
+                    }
+                }
+
+                loop();
+            }
+
+            //Accessories
+            Task.Factory.StartNew(() => { getAndCompileDataInCategory(11, 54, 0, 0, 1); }); //Head
+            Task.Factory.StartNew(() => { getAndCompileDataInCategory(11, 21, 0, 1, 2); }); //Face
+            Task.Factory.StartNew(() => { getAndCompileDataInCategory(11, 22, 0, 2, 3); }); //Neck
+            Task.Factory.StartNew(() => { getAndCompileDataInCategory(11, 23, 0, 3, 4); }); //Shoulder
+            Task.Factory.StartNew(() => { getAndCompileDataInCategory(11, 24, 0, 4, 5); }); //Front
+            Task.Factory.StartNew(() => { getAndCompileDataInCategory(11, 25, 0, 5, 6); }); //Back
+            Task.Factory.StartNew(() => { getAndCompileDataInCategory(11, 26, 0, 6, 7); }); //Waist
+
+            //Layered Clothing
+            Task.Factory.StartNew(() => { getAndCompileDataInCategory(3, 62, 1, 0, 8); }); //Sweaters
+            Task.Factory.StartNew(() => { getAndCompileDataInCategory(3, 61, 1, 1, 9); }); //Jackets
+            Task.Factory.StartNew(() => { getAndCompileDataInCategory(3, 60, 1, 2, 10); }); //Pants
+            Task.Factory.StartNew(() => { getAndCompileDataInCategory(3, 63, 1, 3, 11); }); //Shorts
+            Task.Factory.StartNew(() => { getAndCompileDataInCategory(3, 65, 1, 4, 12); }); //Dresses & Skirts
+            Task.Factory.StartNew(() => { getAndCompileDataInCategory(3, 64, 1, 5, 13); }); //Shoes
+            Task.Factory.StartNew(() => { getAndCompileDataInCategory(3, 59, 1, 6, 14); }); //Shirts
+            Task.Factory.StartNew(() => { getAndCompileDataInCategory(3, 58, 1, 7, 15); }); //T-Shirts
+
+            //Body
+            Task.Factory.StartNew(() => { getAndCompileDataInCategory(4, 20, 2, 2, 16); }); //Hair
+            Task.Factory.StartNew(() => { getAndCompileDataInCategory(4, 10, 2, 3, 17); }); //Faces
+            Task.Factory.StartNew(() => { getAndCompileDataInCategory(17, 0, 2, 1, 18); }); //Bundles
+            Task.Factory.StartNew(() => { getAndCompileDataInCategory(12, 38, 2, 0, 19); }); //Animation Bundles
+            
+            void updateText()
+            {
+                while (true)
+                {
+                    if (currentNumber > max)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        progress_done.Invoke(new MethodInvoker(delegate { progress_done.Text = ("Done: " + currentNumber.ToString() + "/" + max.ToString()); }));
+                        progress_running.Invoke(new MethodInvoker(delegate { progress_running.Text = ("Running: Categ: " + currentCategory.ToString() + ", Sub Categ: " + currentSubCategory.ToString() + ", Page: " + currentPage.ToString()); }));
+                    }
+
+                    if (stalling)
+                    {
+                        stallCounter += 1;
+
+                        result_text.Invoke(new MethodInvoker(delegate { result_text.Text = "Stalled " + stallCounter.ToString() + " times"; }));
+                    }
+                    else
+                    {
+                        stallCounter = 0;
+
+                        result_text.Invoke(new MethodInvoker(delegate { result_text.Text = "Gathering data."; }));
+                    }
+
+                    Thread.Sleep(500);
+                }
+            }
+
+            Thread updating = new Thread(updateText);
+            updating.Start();
+
+            void finishWait()
+            {
+                while (!finished) { Thread.Sleep(100); }
+
+                //Compile our results.
+                dataFinished = @"{
+                    ['Accessories'] = {
+                        ['Head'] = {
+                            " + accessories[0] + @"
+                        },
+
+		                ['Face'] = {
+                            " + accessories[1] + @"
+                        },
+
+		                ['Neck'] = {
+                            " + accessories[2] + @"
+                        },
+
+		                ['Shoulder'] = {
+                            " + accessories[3] + @"
+                        },
+
+		                ['Front'] = {
+                            " + accessories[4] + @"
+                        },
+
+		                ['Back'] = {
+                            " + accessories[5] + @"
+                        },
+
+		                ['Waist'] = {
+                            " + accessories[6] + @"
+                        }
+                    },
+
+                    ['Body'] = {
+                        ['Animation_Packs'] = {
+                            " + body[0] + @"
+                        },
+
+                        ['Bundles'] = {
+                            " + body[1] + @"
+                        },
+
+                        ['Hair'] = {
+                            " + body[2] + @"
+                        },
+
+		                ['Faces'] = {
+                            " + body[3] + @"
+                        }
+                    },
+                
+		            ['Layered'] = {
+			            ['Sweaters'] = {
+                            " + layeredClothing[0] + @"
+			            },
+
+			            ['Jackets'] = {
+                            " + layeredClothing[1] + @"
+			            },
+
+			            ['Shorts'] = {
+                            " + layeredClothing[3] + @"
+			            },
+
+			            ['Dresses_Skirts'] = {
+                            " + layeredClothing[4] + @"
+			            },
+
+			            ['Shoes'] = {
+                            " + layeredClothing[5] + @"
+			            },
+
+			            ['T_Shirts'] = {
+                            " + layeredClothing[7] + @"
+			            },
+
+			            ['Shirts'] = {
+                            " + layeredClothing[6] + @"
+			            },
+
+			            ['Pants'] = {
+                            " + layeredClothing[2] + @"
+			            }
+	                }
+                }";
+
+                running = false;
+
+                result_text.Invoke(new MethodInvoker(delegate { result_text.Text = "Complete, press copy result for a formatted table."; }));
+            }
+
+            Thread finish = new Thread(finishWait);
+            finish.Start();
+        }
+
+        //Check
+
+
+        //Other buttons
+        private void button_run_Click(object sender, EventArgs e)
+        {
+            if (!running)
+            {
+                cancelled = false;
+
+                run();
+            }
+        }
+
+        private void debugMode_box_CheckedChanged(object sender, EventArgs e)
+        {
+            debugMode = debugMode_box.Checked;
+        }
+
+        private void button_copy_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(dataFinished);
+        }
+
+        private void button_cancel_Click(object sender, EventArgs e)
+        {
+            if (running)
+            {
+                cancelled = true;
+            }
+        }
+    }
+}
